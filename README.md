62b83a56f8b013d13939ccfcc6e09086a129186b
wheatandcat <wheatandcat@gmail.com>
2021-07-01 22:03:20 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
+# memoir-notification
+
+Push通知を送信する
 
 
 ## デプロイ
 
 ```
-$ gcloud functions deploy SendNotification --runtime go113 --trigger-http
+$ gcloud functions deploy SendNotification --runtime go113 --trigger-http --region asia-northeast1
+```
+
+
+## Cloud TasksにQueueを登録
+
+
+```
+$ gcloud tasks queues create sendNotification
+```
+
+
+```
+$ gcloud tasks queues update sendNotification --max-attempts 6 --min-backoff 5s --max-doublings 3
+```
+
+## Cloud TasksのQueueを確認
+
+```
+$ gcloud tasks queues describe sendNotification
 ```
--- function.go
+++ function.go
@@ -7,32 +7,35 @@ import (
 	expo "github.com/oliveroneill/exponent-server-sdk-golang/sdk"
 )
 
-type RequestParam struct {
-	Token     string `json:"token"`
-	Title     string `json:"title"`
-	Body      string `json:"body"`
-	URLScheme string `json:"urlScheme"`
+type NotificationRequest struct {
+	Token     []string `json:"token"`
+	Title     string   `json:"title"`
+	Body      string   `json:"body"`
+	URLScheme string   `json:"urlScheme"`
 }
 
 func SendNotification(w http.ResponseWriter, r *http.Request) {
-	param := RequestParam{}
+	param := NotificationRequest{}
 
 	if err := json.NewDecoder(r.Body).Decode(&param); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	pushToken, err := expo.NewExponentPushToken(param.Token)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	to := []expo.ExponentPushToken{}
+	for _, token := range param.Token {
+		pushToken, err := expo.NewExponentPushToken(token)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		to = append(to, pushToken)
 
-	to := []expo.ExponentPushToken{pushToken}
+	}
 
 	client := expo.NewPushClient(nil)
 
-	_, err = client.Publish(
+	_, err := client.Publish(
 		&expo.PushMessage{
 			To:       to,
 			Body:     param.Body,
